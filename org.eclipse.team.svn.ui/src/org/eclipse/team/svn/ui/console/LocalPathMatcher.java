6f5f6fd7d11e60358c91a082dd88f4de02533d80
Alexander Gurov <a.gurov@i-ween.com>
2007-11-30 14:26:49 +0000

    bug 207953: CPU hogging https://bugs.eclipse.org/bugs/show_bug.cgi?id=207953

--- org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/console/LocalPathMatcher.java
+++ org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/console/LocalPathMatcher.java
@@ -10,17 +10,13 @@
  *******************************************************************************/
 package org.eclipse.team.svn.ui.console;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.team.svn.core.SVNTeamPlugin;
 import org.eclipse.team.svn.core.operation.AbstractNonLockingOperation;
 import org.eclipse.team.svn.ui.SVNTeamUIPlugin;
 import org.eclipse.team.svn.ui.preferences.SVNTeamPreferences;
@@ -36,62 +32,22 @@ import org.eclipse.ui.console.TextConsole;
  * @author Alexey Mikoyan
  *
  */
+
 public class LocalPathMatcher implements IPatternMatchListenerDelegate, IPropertyChangeListener {
 
-	protected Pattern[] patterns = new Pattern[0];
+	protected Pattern pattern;
 	
 	protected TextConsole console;
 	protected boolean enabled;
 	
-	public void createPatterns() {
-		ArrayList patternList = new ArrayList();
-		
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Added"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Modified"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Deleted"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Missing"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Replaced"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Merged"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Conflicted"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Status.Obstructed"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Added"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Deleted"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Modified"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Replaced"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Reverted"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Restored"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Locked"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Action.Unlocked"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.inapplicable"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.unknown"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.unchanged"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.missing"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.obstructed"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.changed"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.merged"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.conflicted"));
-		this.addPattern(patternList, SVNTeamPlugin.instance().getResource("Console.Update.Status.conflicted_unresolved"));
-		
-		this.patterns = (Pattern[])patternList.toArray(new Pattern[patternList.size()]);
-	}
-	
-	protected void addPattern(List patterns, String base) {
-		if (base.length() == 0) {
-			return;
-		}
-		String template = ".*\\s+";
-		template += "\\Q" + base + "\\E";
-		template += "\\s+([^\r\n]+)";
-		try {
-			patterns.add(Pattern.compile(template));
-		}
-		catch (PatternSyntaxException pse) {
-		}
+	public void createPattern() {
+		String regExp = "(?:[A-Z]\\:)?(?:[\\\\/][^\\\\/\\:\\?\\*\r\n\"]+)+";
+		this.pattern = Pattern.compile(regExp);
 	}
 	
 	public void connect(TextConsole console) {
 		this.console = console;
-		this.createPatterns();
+		this.createPattern();
 		this.loadPreferences();
 		SVNTeamUIPlugin.instance().getPreferenceStore().addPropertyChangeListener(this);
 	}
@@ -134,20 +90,12 @@ public class LocalPathMatcher implements IPatternMatchListenerDelegate, IPropert
 			if (path == null) {
 				return;
 			}
-		    int start = 0;
-			for (int i = 0; i < LocalPathMatcher.this.patterns.length; i++) {
-	    	    Pattern pattern = LocalPathMatcher.this.patterns[i];
-	    	    Matcher matcher = pattern.matcher(path);
-	    	    while(matcher.find(start)) {
-					length = matcher.end(1) - matcher.start(1);
-					String link = path.substring(matcher.start(1), matcher.end(1));
-					if (link != null) {
-						LocalPathMatcher.this.console.addHyperlink(new LocalFileHyperlink(link), offset + matcher.start(1), length);							
-						return;
-					}
-					start = matcher.end();
-	    	    }
-			}
+			
+	    	Matcher matcher = LocalPathMatcher.this.pattern.matcher(path);
+	    	if (matcher.find(0)) {
+				String link = matcher.group(matcher.groupCount()).trim();
+				LocalPathMatcher.this.console.addHyperlink(new LocalFileHyperlink(link), offset + matcher.start(matcher.groupCount()), link.length());
+	    	}
 		}
 		
 	}
