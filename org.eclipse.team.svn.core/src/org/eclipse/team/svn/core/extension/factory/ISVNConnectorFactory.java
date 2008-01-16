abf06fd55c5128d38d3d81b4009c50ae8e2414d9
Alexei Goncharov <alexei.goncharov@polarion.org>
2008-01-16 15:39:20 +0000

    Build numbers were changed

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/extension/factory/ISVNConnectorFactory.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/extension/factory/ISVNConnectorFactory.java
@@ -22,7 +22,7 @@ import org.eclipse.team.svn.core.operation.UnreportableException;
  */
 public interface ISVNConnectorFactory {
 	public static final String DEFAULT_ID = "org.eclipse.team.svn.connector.svnkit";
-	public static final String CURRENT_COMPATIBILITY_VERSION = "0.7.0.v20080111";
+	public static final String CURRENT_COMPATIBILITY_VERSION = "0.7.0.v20080116";
 	
 	/**
 	 * Enumeration of connector API compatibility levels
