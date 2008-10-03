ec6ebfcf1cd64bda74b89393957234633d4a2ec9
Alexei Goncharov <alexei.goncharov@polarion.org>
2008-10-03 13:28:51 +0000

    Build id's, release notes and change log are changed

--- org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/wizard/shareproject/SelectProjectNamePage.java
+++ org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/wizard/shareproject/SelectProjectNamePage.java
@@ -128,8 +128,6 @@ public class SelectProjectNamePage extends AbstractVerifiedWizardPage {
 	protected Composite createControlImpl(Composite parent) {																	
 		Composite composite = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
-		//layout.marginWidth = 0;
-		//layout.marginHeight = 0;		
 		composite.setLayout(layout);
 		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
