c98e49299ce16612110e365e86bd33fd3d300347
Igor Burilo <igor.burilo@polarion.org>
2009-01-15 11:53:29 +0000

    NEW - bug 254900: Resources list isn't updated in "Override and Update..." https://bugs.eclipse.org/bugs/show_bug.cgi?id=254900

--- org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/panel/local/OverrideResourcesPanel.java
+++ org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/panel/local/OverrideResourcesPanel.java
@@ -12,7 +12,9 @@
 package org.eclipse.team.svn.ui.panel.local;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.dialogs.IDialogConstants;
@@ -21,10 +23,16 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.team.svn.core.IStateFilter;
+import org.eclipse.team.svn.core.resource.events.IResourceStatesListener;
+import org.eclipse.team.svn.core.resource.events.ResourceStatesChangedEvent;
+import org.eclipse.team.svn.core.svnstorage.SVNRemoteStorage;
+import org.eclipse.team.svn.core.utility.FileUtility;
 import org.eclipse.team.svn.ui.SVNUIMessages;
 import org.eclipse.team.svn.ui.composite.ResourceSelectionComposite;
 import org.eclipse.team.svn.ui.panel.participant.BasePaneParticipant;
 import org.eclipse.team.svn.ui.synchronize.AbstractSynchronizeActionGroup;
+import org.eclipse.team.svn.ui.utility.UIMonitorUtility;
 import org.eclipse.team.svn.ui.verifier.AbstractVerifier;
 import org.eclipse.team.ui.synchronize.ResourceScope;
 
@@ -38,6 +46,8 @@ public class OverrideResourcesPanel extends AbstractResourceSelectionPanel {
 	public static final int MSG_UPDATE = 1;
 	
 	protected IResource [] affectedResource;
+
+	protected IResourceStatesListener resourceStatesListener;
 	
 	protected static final String []MESSAGES = new String[] {
 		"OverrideResourcesPanel_Description_Commit", //$NON-NLS-1$
@@ -91,11 +101,51 @@ public class OverrideResourcesPanel extends AbstractResourceSelectionPanel {
 		});
 	}
 	
+	protected void updateResources(ResourceStatesChangedEvent event) {
+		HashSet<IResource> allResources = new HashSet<IResource>(Arrays.asList(this.resources));
+		
+		HashSet<IResource> toDeleteSet = new HashSet<IResource>();
+		toDeleteSet.addAll(Arrays.asList(FileUtility.getResourcesRecursive(event.resources, IStateFilter.SF_NOTMODIFIED, IResource.DEPTH_ZERO)));
+		toDeleteSet.addAll(Arrays.asList(FileUtility.getResourcesRecursive(event.resources, IStateFilter.SF_NOTEXISTS, IResource.DEPTH_ZERO)));
+		toDeleteSet.addAll(Arrays.asList(FileUtility.getResourcesRecursive(event.resources, IStateFilter.SF_IGNORED, IResource.DEPTH_ZERO)));
+		
+		allResources.removeAll(toDeleteSet);
+		
+		final IResource[] newResources = allResources.toArray(new IResource[allResources.size()]);
+		
+		if (!this.paneParticipantHelper.isParticipantPane()) {
+			UIMonitorUtility.getDisplay().syncExec(new Runnable() {
+				public void run() {
+					//FIXME isDisposed() test is necessary as dispose() method is not called from FastTrack Commit Dialog
+					if (!OverrideResourcesPanel.this.selectionComposite.isDisposed()) {
+						OverrideResourcesPanel.this.selectionComposite.setResources(newResources);
+						OverrideResourcesPanel.this.selectionComposite.fireSelectionChanged();
+					}
+				}
+			});	
+		}
+		
+		this.resources = newResources;
+	}	
+	
 	public void postInit() {
 	   	super.postInit();
 	   	this.validateContent();
+	   	
+	   	this.resourceStatesListener = new IResourceStatesListener() {
+			public void resourcesStateChanged(ResourceStatesChangedEvent event) {
+				OverrideResourcesPanel.this.updateResources(event);
+			}
+		};
+		SVNRemoteStorage.instance().addResourceStatesListener(ResourceStatesChangedEvent.class, OverrideResourcesPanel.this.resourceStatesListener);
 	}
     
+	public void dispose() {
+    	super.dispose();
+    	
+    	SVNRemoteStorage.instance().removeResourceStatesListener(ResourceStatesChangedEvent.class, this.resourceStatesListener);	    	    	
+    }
+	
     public String getHelpId() {
     	return "org.eclipse.team.svn.help.overrideDialogContext"; //$NON-NLS-1$
     }
