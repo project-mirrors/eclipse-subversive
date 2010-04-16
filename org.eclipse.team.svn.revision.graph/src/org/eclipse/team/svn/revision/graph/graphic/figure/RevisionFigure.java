1d5b865e8452c32578fa34df3aceafef0452fc80
Igor Burilo <igor.burilo@polarion.org>
2010-04-16 14:57:46 +0000

    Improve nodes presentation: customize images, colors etc.

--- org.eclipse.team.svn.revision.graph/src/org/eclipse/team/svn/revision/graph/graphic/figure/RevisionFigure.java
+++ org.eclipse.team.svn.revision.graph/src/org/eclipse/team/svn/revision/graph/graphic/figure/RevisionFigure.java
@@ -13,12 +13,15 @@ package org.eclipse.team.svn.revision.graph.graphic.figure;
 import org.eclipse.draw2d.Border;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.FlowLayout;
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.GridData;
 import org.eclipse.draw2d.GridLayout;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.PositionConstants;
-import org.eclipse.draw2d.SchemeBorder;
-import org.eclipse.draw2d.SchemeBorder.Scheme;
+import org.eclipse.draw2d.RoundedRectangle;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.draw2d.text.FlowPage;
 import org.eclipse.draw2d.text.ParagraphTextLayout;
 import org.eclipse.draw2d.text.TextFlow;
@@ -28,6 +31,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.team.svn.core.SVNMessages;
+import org.eclipse.team.svn.revision.graph.SVNRevisionGraphMessages;
 import org.eclipse.team.svn.revision.graph.SVNRevisionGraphPlugin;
 import org.eclipse.team.svn.revision.graph.PathRevision.ReviosionNodeType;
 import org.eclipse.team.svn.revision.graph.PathRevision.RevisionNodeAction;
@@ -39,7 +43,7 @@ import org.eclipse.team.svn.ui.utility.UIMonitorUtility;
  * 
  * @author Igor Burilo
  */
-public class RevisionFigure extends Figure {
+public class RevisionFigure extends RoundedRectangle {
 
 	protected final static int FIGURE_WIDTH = 200;
 			
@@ -69,6 +73,7 @@ public class RevisionFigure extends Figure {
 	protected Border originalBorder;
 	
 	protected Label revisionFigure;
+	protected Label statusFigure;
 	protected TextFlow pathTextFlow;
 	protected Label commentFigure;
 	
@@ -95,32 +100,32 @@ public class RevisionFigure extends Figure {
 		RENAME_IMAGE = SVNRevisionGraphPlugin.instance().getImageDescriptor("icons/rename.gif").createImage(); //$NON-NLS-1$
 		SVNRevisionGraphPlugin.disposeOnShutdown(RENAME_IMAGE);
 		
-		OTHER_IMAGE = SVNRevisionGraphPlugin.instance().getImageDescriptor("icons/other.gif").createImage(); //$NON-NLS-1$
+		OTHER_IMAGE = SVNRevisionGraphPlugin.instance().getImageDescriptor("icons/other.png").createImage(); //$NON-NLS-1$
 		SVNRevisionGraphPlugin.disposeOnShutdown(OTHER_IMAGE);
 		
 		//colors
-		TRUNK_COLOR = new Color(UIMonitorUtility.getDisplay(), 0, 255, 128);
+		TRUNK_COLOR = new Color(UIMonitorUtility.getDisplay(), 188, 255, 188);
 		SVNRevisionGraphPlugin.disposeOnShutdown(TRUNK_COLOR);
 		
-		BRANCH_COLOR = new Color(UIMonitorUtility.getDisplay(), 84, 159, 252);
+		BRANCH_COLOR = new Color(UIMonitorUtility.getDisplay(), 229, 255, 229);
 		SVNRevisionGraphPlugin.disposeOnShutdown(BRANCH_COLOR);
 		
-		TAG_COLOR = new Color(UIMonitorUtility.getDisplay(), 255, 255, 128);
+		TAG_COLOR = new Color(UIMonitorUtility.getDisplay(), 239, 252, 162);
 		SVNRevisionGraphPlugin.disposeOnShutdown(TAG_COLOR);
 		
-		CREATE_OR_COPY_COLOR = new Color(UIMonitorUtility.getDisplay(), 198, 255, 198);
+		CREATE_OR_COPY_COLOR = new Color(UIMonitorUtility.getDisplay(), 229, 255, 229);
 		SVNRevisionGraphPlugin.disposeOnShutdown(CREATE_OR_COPY_COLOR);
 		
-		RENAME_COLOR = new Color(UIMonitorUtility.getDisplay(), 128, 128, 255);
+		RENAME_COLOR = new Color(UIMonitorUtility.getDisplay(), 229, 229, 255);
 		SVNRevisionGraphPlugin.disposeOnShutdown(RENAME_COLOR);
 		
-		DELETE_COLOR = new Color(UIMonitorUtility.getDisplay(), 255, 140, 140);
+		DELETE_COLOR = new Color(UIMonitorUtility.getDisplay(), 255, 229, 229);
 		SVNRevisionGraphPlugin.disposeOnShutdown(DELETE_COLOR);
 		
-		MODIFY_COLOR = new Color(UIMonitorUtility.getDisplay(), 192, 192, 192);
+		MODIFY_COLOR = new Color(UIMonitorUtility.getDisplay(), 229, 229, 229);
 		SVNRevisionGraphPlugin.disposeOnShutdown(MODIFY_COLOR);
 		
-		OTHER_COLOR = new Color(UIMonitorUtility.getDisplay(), 245, 245, 245);
+		OTHER_COLOR = new Color(UIMonitorUtility.getDisplay(), 240, 240, 240);
 		SVNRevisionGraphPlugin.disposeOnShutdown(OTHER_COLOR);
 		
 		SELECTED_COLOR = new Color(UIMonitorUtility.getDisplay(), 47, 104, 200);
@@ -133,30 +138,36 @@ public class RevisionFigure extends Figure {
 		
 		this.createControls();								
 		this.initControls();
-						
-		this.setBorder(this.originalBorder = new SchemeBorder(new Scheme(
-				new Color[]{ColorConstants.button},
-				new Color[]{ColorConstants.buttonDarkest})));
 		
 		//non-transparent
 		this.setOpaque(true);
 	}
 	
 	protected void createControls() {		
+		
+		this.setCornerDimensions(new Dimension(8, 8));
+		
 		GridLayout layout = new GridLayout();		
 		//layout.marginHeight = layout.marginWidth = 2;
 		//layout.horizontalSpacing = layout.verticalSpacing = 3; 		
 		this.setLayoutManager(layout);												
+							
+		Figure revisionParent = new Figure();
+		this.add(revisionParent);		
+		FlowLayout revisionLayout = new FlowLayout(true);
+		revisionParent.setLayoutManager(revisionLayout);
+		GridData data = new GridData();
+		layout.setConstraint(revisionParent, data);
 		
 		this.revisionFigure = new Label();
-		this.add(revisionFigure);
-		GridData data = new GridData();
-		data.horizontalAlignment = SWT.CENTER;
-		data.grabExcessHorizontalSpace = true;
-		layout.setConstraint(this.revisionFigure, data);		
+		revisionParent.add(this.revisionFigure);		
 		Font boldFont = JFaceResources.getFontRegistry().getBold(JFaceResources.DEFAULT_FONT);
 		this.revisionFigure.setFont(boldFont);		
-
+		
+		//status
+		this.statusFigure = new Label();
+		revisionParent.add(this.statusFigure);		
+				
 		//path
 		if (this.revisionNode.getAction() == RevisionNodeAction.ADD || 
 			this.revisionNode.getAction() == RevisionNodeAction.COPY ||
@@ -186,8 +197,34 @@ public class RevisionFigure extends Figure {
 		this.commentFigure.setLabelAlignment(PositionConstants.LEFT);
 	}			
 	
+	protected void outlineShape(Graphics graphics) {
+		//draw border in corresponding to revision node color		
+		Color color = getRevisionNodeBorderColor(this.revisionNode);				
+		graphics.setForegroundColor(color);
+		
+		super.outlineShape(graphics);
+	}
+	
+	@Override
+	protected void fillShape(Graphics graphics) {		
+		//add shadow		
+		final int shadow = 2;
+		final Rectangle bounds = getBounds();
+		graphics.pushState();
+		graphics.setBackgroundColor(ColorConstants.gray);
+		graphics.setAlpha(140);
+		graphics.setClip(new Rectangle(bounds.x, bounds.y, bounds.width+shadow, bounds.height + shadow));
+		final Rectangle shadowRect = bounds.getCopy().translate(shadow, shadow);
+		graphics.fillRoundRectangle(shadowRect, corner.width, corner.height);
+		graphics.popState();
+		
+		super.fillShape(graphics);
+	}
+	
 	protected void initControls() {
 		this.revisionFigure.setText(String.valueOf(this.revisionNode.getRevision()));
+				
+		this.statusFigure.setText(RevisionFigure.getRevisionNodeStatusText(this.revisionNode));
 		
 		if (this.pathTextFlow != null) {
 			this.pathTextFlow.setText(this.path);	
@@ -216,16 +253,65 @@ public class RevisionFigure extends Figure {
 		if (isSelected) {
 			this.setBackgroundColor(SELECTED_COLOR);
 			this.setForegroundColor(ColorConstants.white);
-			//this.setBorder(new FocusBorder());
 		} else {
 			this.setBackgroundColor(this.originalBgColor);
 			this.setForegroundColor(ColorConstants.black);
-			//this.setBorder(this.originalBorder);
 		}		
 	}	
 	
 	public RevisionNode getRevisionNode() {
 		return this.revisionNode;
+	}
+	
+	public static Color getRevisionNodeBorderColor(RevisionNode revisionNode) {
+	    ReviosionNodeType type = revisionNode.getType();
+		RevisionNodeAction action = revisionNode.getAction();		
+	    Color color;	   	    
+		if (ReviosionNodeType.TRUNK.equals(type)) {
+			color = ColorConstants.green;			
+		} else if (ReviosionNodeType.BRANCH.equals(type)) {
+			color = ColorConstants.green;
+		} else if (ReviosionNodeType.TAG.equals(type)) {
+			color = ColorConstants.black;
+		} else if (RevisionNodeAction.ADD.equals(action) || RevisionNodeAction.COPY.equals(action)) {
+			color = ColorConstants.green;
+		} else if (RevisionNodeAction.RENAME.equals(action)) {
+			color = ColorConstants.blue;
+		} else if (RevisionNodeAction.DELETE.equals(action)) {
+			color = ColorConstants.red;
+		} else if (RevisionNodeAction.MODIFY.equals(action)) {
+			color = ColorConstants.black;
+		} else {
+			color = ColorConstants.black;
+		}
+		return color;
+	}
+	
+	public static String getRevisionNodeStatusText(RevisionNode revisionNode) {
+	    ReviosionNodeType type = revisionNode.getType();
+		RevisionNodeAction action = revisionNode.getAction();		
+	    String text = "[";	   	     //$NON-NLS-1$
+		if (ReviosionNodeType.TRUNK.equals(type)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Trunk;
+		} else if (ReviosionNodeType.BRANCH.equals(type)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Branch;
+		} else if (ReviosionNodeType.TAG.equals(type)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Tag;
+		} else if (RevisionNodeAction.ADD.equals(action)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Create;
+		} else if (RevisionNodeAction.COPY.equals(action)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Copy;
+		} else if (RevisionNodeAction.RENAME.equals(action)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Rename;
+		} else if (RevisionNodeAction.DELETE.equals(action)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Delete;
+		} else if (RevisionNodeAction.MODIFY.equals(action)) {
+			text += SVNRevisionGraphMessages.RevisionFigure_Edit;
+		} else {
+			text += SVNRevisionGraphMessages.RevisionFigure_NoChanges;
+		}
+		text += "]"; //$NON-NLS-1$
+		return text;
 	}
 	
 	public static Color getRevisionNodeColor(RevisionNode revisionNode) {
