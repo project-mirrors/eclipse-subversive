3ade8c72b4a7fc93e769738db2e4d1bf2b9c2ba9
Alexander Gurov <a.gurov@i-ween.com>
2011-03-21 16:15:19 +0000

    Correct connectors compatibility ID [ID-D1U3Y] (bug 340527) https://bugs.eclipse.org/bugs/show_bug.cgi?id=340527

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/extension/factory/ISVNConnectorFactory.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/extension/factory/ISVNConnectorFactory.java
@@ -22,7 +22,7 @@ import org.eclipse.team.svn.core.operation.UnreportableException;
  */
 public interface ISVNConnectorFactory {
 	public static final String DEFAULT_ID = "org.eclipse.team.svn.connector.svnkit15"; //$NON-NLS-1$
-	public static final String CURRENT_COMPATIBILITY_VERSION = "0.7.9.I20110101-1700"; //$NON-NLS-1$
+	public static final String CURRENT_COMPATIBILITY_VERSION = "0.7.9.I20110318-1700"; //$NON-NLS-1$
 	
 	/**
 	 * Enumeration of connector API compatibility levels
