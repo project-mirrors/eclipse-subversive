f496389451412d7a148a7b0572be09d624a3b27f
Alexander Gurov <a.gurov@i-ween.com>
2011-06-17 16:03:03 +0000

    Deleting and replacing a resource is a SVN delete/add instead of a modify (bug 276018) [slight correction to the current implementation] https://bugs.eclipse.org/bugs/show_bug.cgi?id=276018

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/FileReplaceListener.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/FileReplaceListener.java
@@ -13,11 +13,9 @@ package org.eclipse.team.svn.core;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
@@ -26,12 +24,10 @@ import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.core.runtime.jobs.MultiRule;
-import org.eclipse.team.svn.core.operation.AbstractActionOperation;
 import org.eclipse.team.svn.core.operation.CompositeOperation;
 import org.eclipse.team.svn.core.operation.IActionOperation;
 import org.eclipse.team.svn.core.operation.LoggedOperation;
+import org.eclipse.team.svn.core.operation.local.AbstractWorkingCopyOperation;
 import org.eclipse.team.svn.core.operation.local.RefreshResourcesOperation;
 import org.eclipse.team.svn.core.operation.local.RevertOperation;
 import org.eclipse.team.svn.core.resource.ILocalResource;
@@ -52,114 +48,90 @@ public class FileReplaceListener implements IResourceChangeListener {
 		try {
 			final List<IFile> added = new ArrayList<IFile>();			
 			event.getDelta().accept(new IResourceDeltaVisitor() {
-				public boolean visit(IResourceDelta delta) throws CoreException {					
-					if (delta.getResource().getType() == IResource.FILE && delta.getKind() == IResourceDelta.ADDED) {
+				public boolean visit(IResourceDelta delta) throws CoreException {
+					// replacement does not send addition event, but CHANGE event is sent in its place
+					if (delta.getResource().getType() == IResource.FILE && (delta.getKind() == IResourceDelta.ADDED || delta.getKind() == IResourceDelta.CHANGED)) {
 						added.add((IFile)delta.getResource());
 					}					
 					return true;
 				}			
 			});			
 			if (!added.isEmpty()) {
-				this.processResources(added);
+				this.processResources(added.toArray(new IResource[0]));
 			}
 		} catch (CoreException e) {
 			LoggedOperation.reportError(this.getClass().getName(), e);
 		}
 	}
 
-	protected void processResources(final List<IFile> addedFiles) {
-		//collect files which were deleted and then added
-		final List<ILocalResource> localResources = new ArrayList<ILocalResource>();
-		final List<IResource> resources = new ArrayList<IResource>();
-		for (IResource resource : addedFiles) {
-			ILocalResource local = SVNRemoteStorage.instance().asLocalResource(resource);								
-			if (IStateFilter.SF_DELETED.accept(local)) {
-				//check if parent directory is not marked for deletion
-				IResource parent = local.getResource().getParent();
-				if (parent != null) {
+	protected void processResources(IResource []resources) {
+		AbstractWorkingCopyOperation mainOp = new AbstractWorkingCopyOperation("Operation_FileReplaceListener", SVNMessages.class, resources) { //$NON-NLS-1$
+			protected void runImpl(IProgressMonitor monitor) throws Exception {																				
+				for (final IResource file : this.operableData()) {														
+					if (monitor.isCanceled()) {
+						return;
+					}				
+
+					ILocalResource local = SVNRemoteStorage.instance().asLocalResource(file);								
+					if (!(IStateFilter.SF_DELETED.accept(local) || IStateFilter.SF_PREREPLACEDREPLACED.accept(local))) {
+						continue;
+					}
+					IResource parent = file.getParent();
 					ILocalResource localParent = SVNRemoteStorage.instance().asLocalResource(parent);
-					if (!IStateFilter.SF_DELETED.accept(localParent)) {
-						localResources.add(local);	
-						resources.add(local.getResource());
+					if (IStateFilter.SF_DELETED.accept(localParent)) {
+						continue;
 					}									
-				}				
-			}						
-		}
-		if (!localResources.isEmpty()) {		
-			final AbstractActionOperation mainOp = new AbstractActionOperation("Operation_FileReplaceListener", SVNMessages.class) { //$NON-NLS-1$
-				protected void runImpl(IProgressMonitor monitor) throws Exception {																				
-					for (final ILocalResource local : localResources) {														
-						if (monitor.isCanceled()) {
-							return;
-						}				
-						
-						boolean hasError = true;
-						File originalFile = new File(FileUtility.getWorkingCopyPath(local.getResource()));
-						File tmpFile = new File(originalFile + ".svntmp"); //$NON-NLS-1$
-						//used for restore
-						File fileWithOriginalContent = originalFile;
-						try {							
-							if (tmpFile.exists()) {
-								tmpFile.delete();
-							}											
-							if (originalFile.renameTo(tmpFile)) {
-								fileWithOriginalContent = tmpFile;
-								RevertOperation revertOp = new RevertOperation(new IResource[]{local.getResource()}, false);
-								ProgressMonitorUtility.doTask(revertOp, monitor, 100, 60);													
-								if (revertOp.getExecutionState() == IActionOperation.OK) {			
-									if (!originalFile.delete()) {
-										throw new Exception("Failed to delete file: " + originalFile); //$NON-NLS-1$
-									}
-									if (!tmpFile.renameTo(originalFile)) {
-										throw new Exception("Failed to rename file: " + originalFile); //$NON-NLS-1$
-									}
-									fileWithOriginalContent = originalFile;
-									hasError = false;
-								} else {
-									this.reportStatus(revertOp.getStatus());
-								}								
+					
+					boolean hasError = true;
+					File originalFile = new File(FileUtility.getWorkingCopyPath(file));
+					File tmpFile = new File(originalFile + ".svntmp"); //$NON-NLS-1$
+					//used for restore
+					File fileWithOriginalContent = originalFile;
+					try {							
+						if (tmpFile.exists()) {
+							tmpFile.delete();
+						}											
+						if (originalFile.renameTo(tmpFile)) {
+							fileWithOriginalContent = tmpFile;
+							RevertOperation revertOp = new RevertOperation(new IResource[]{file}, false);
+							ProgressMonitorUtility.doTask(revertOp, monitor, 100, 60);													
+							if (revertOp.getExecutionState() == IActionOperation.OK) {			
+								if (!originalFile.delete()) {
+									throw new Exception("Failed to delete file: " + originalFile); //$NON-NLS-1$
+								}
+								if (!tmpFile.renameTo(originalFile)) {
+									throw new Exception("Failed to rename file: " + originalFile); //$NON-NLS-1$
+								}
+								fileWithOriginalContent = originalFile;
+								hasError = false;
 							} else {
-								throw new Exception("Failed to rename file: " + originalFile.getAbsolutePath()); //$NON-NLS-1$
-							}
-						} catch (Throwable t) {
-							this.reportStatus(IStatus.ERROR, null, t);
-						} finally {
-							/*
-							 * Restore
-							 */
-							if (hasError) {
-								if (fileWithOriginalContent.equals(tmpFile)) {
-									if (originalFile.exists()) {
-										originalFile.delete();
-									}
-									if (tmpFile.renameTo(originalFile)) {
-										tmpFile.delete();
-									}
-								} else if (fileWithOriginalContent.equals(originalFile)) {
-									if (tmpFile.exists()) {
-										tmpFile.delete();	
-									}									
-								}								
-							}
+								this.reportStatus(revertOp.getStatus());
+							}								
+						} else {
+							throw new Exception("Failed to rename file: " + originalFile.getAbsolutePath()); //$NON-NLS-1$
 						}
-					}									
-				}	
-				
-				public ISchedulingRule getSchedulingRule() {				
-			    	HashSet<IResource> ruleSet = new HashSet<IResource>();	
-			    	ruleSet.addAll(resources);
-			    	
-			    	for (IResource resource : resources) {
-						ruleSet.add(resource instanceof IProject ? resource : resource.getParent());
-			    	}
-			    	return new MultiRule(ruleSet.toArray(new IResource[ruleSet.size()]));
-				}
-			};			
-			
-			CompositeOperation cmpOp = new CompositeOperation(mainOp.getId(), mainOp.getMessagesClass());
-			cmpOp.add(mainOp);
-			cmpOp.add(new RefreshResourcesOperation(resources.toArray(new IResource[0])));			
-			ProgressMonitorUtility.doTaskScheduledDefault(cmpOp);
-		}				
+					} catch (Throwable t) {
+						this.reportStatus(IStatus.ERROR, null, t);
+					} finally {
+						/*
+						 * Restore
+						 */
+						if (hasError) {
+							if (fileWithOriginalContent.equals(tmpFile)) {
+								originalFile.delete();
+								tmpFile.renameTo(originalFile);
+							} else if (fileWithOriginalContent.equals(originalFile)) {
+								tmpFile.delete();	
+							}								
+						}
+					}
+				}									
+			}	
+		};			
+		
+		CompositeOperation cmpOp = new CompositeOperation(mainOp.getId(), mainOp.getMessagesClass());
+		cmpOp.add(mainOp);
+		cmpOp.add(new RefreshResourcesOperation(resources));			
+		ProgressMonitorUtility.doTaskScheduledDefault(cmpOp);
 	}
 }
