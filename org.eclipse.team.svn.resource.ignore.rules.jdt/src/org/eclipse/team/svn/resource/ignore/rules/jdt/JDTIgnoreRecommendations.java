f49a491cf6f7a3aa877c9a526e932014d6c2d58e
Alexander Gurov <a.gurov@i-ween.com>
2011-11-14 09:19:25 +0000

    "Subversive JDT ignore recommendations" plug-in deadlocks on Eclipse startup (bug 359199) https://bugs.eclipse.org/bugs/show_bug.cgi?id=359199

--- org.eclipse.team.svn.resource.ignore.rules.jdt/src/org/eclipse/team/svn/resource/ignore/rules/jdt/JDTIgnoreRecommendations.java
+++ org.eclipse.team.svn.resource.ignore.rules.jdt/src/org/eclipse/team/svn/resource/ignore/rules/jdt/JDTIgnoreRecommendations.java
@@ -15,6 +15,8 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IOpenable;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.team.svn.core.extension.options.IIgnoreRecommendations;
@@ -39,14 +41,24 @@ public class JDTIgnoreRecommendations implements IIgnoreRecommendations {
 		if (project == null) {
 			return false;
 		}
-		IPath output = JavaCore.create(project).getOutputLocation();
-		IPackageFragmentRoot []roots = JavaCore.create(project).getPackageFragmentRoots();
-		for (int i = 0; i < roots.length; i++) {
-			if (output.isPrefixOf(roots[i].getPath())) {
-				return false;
+		IJavaProject javaProject = JavaCore.create(project);
+		IPath output = javaProject.getOutputLocation();
+		// if this resource not in the output folder or the project itself is the output folder then no need to ignore it
+		if (!output.isPrefixOf(resource.getFullPath()) || output.equals(project.getFullPath())) {
+			return false;
+		}
+		if (!"bin".equals(output.lastSegment())) { //default folder name used in eclipse while creating project with separate source and binary folders
+			IOpenable openable = javaProject.getOpenable();
+			if (openable.isOpen()) { // do not start any time consuming process
+				IPackageFragmentRoot []roots = JavaCore.create(project).getPackageFragmentRoots();
+				for (int i = 0; i < roots.length; i++) {
+					if (output.isPrefixOf(roots[i].getPath())) {
+						return false;
+					}
+				}
 			}
 		}
-		return output.isPrefixOf(resource.getFullPath());
+		return true;
 	}
 
 }
