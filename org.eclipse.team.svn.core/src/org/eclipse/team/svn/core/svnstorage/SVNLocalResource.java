26a26af25f188282cdb3f18bafc96ac8d3219503
Alexander Gurov <a.gurov@i-ween.com>
2012-03-09 17:33:25 +0000

    NPE in EditTreeConflictsPanel due to wrong enablement (bug 373821) https://bugs.eclipse.org/bugs/show_bug.cgi?id=373821

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/svnstorage/SVNLocalResource.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/svnstorage/SVNLocalResource.java
@@ -93,7 +93,7 @@ public abstract class SVNLocalResource implements ILocalResource {
 	}
 
 	public boolean hasTreeConflict() {
-		return this.treeConflictDescriptor != null;
+		return this.treeConflictDescriptor != null && this.treeConflictDescriptor.conflictKind == SVNConflictDescriptor.Kind.TREE;
 	}
 	
 	public SVNConflictDescriptor getTreeConflictDescriptor() {
