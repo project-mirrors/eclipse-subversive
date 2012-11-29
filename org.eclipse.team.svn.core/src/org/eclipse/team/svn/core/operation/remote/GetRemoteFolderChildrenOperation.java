f27186432b02a737ca134726fe85422e335b5539
Alexander Gurov <a.gurov@i-ween.com>
2012-11-29 17:44:45 +0000

    Human/Numeric ordering option for branches e tags (bug 390467) https://bugs.eclipse.org/bugs/show_bug.cgi?id=390467

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/operation/remote/GetRemoteFolderChildrenOperation.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/operation/remote/GetRemoteFolderChildrenOperation.java
@@ -8,6 +8,7 @@
  * Contributors:
  *    Alexander Gurov - Initial API and implementation
  *    Julien HENRY - fix for the issue #350143 (svn:externals should not be treated as IRepositoryRoot entries)
+ *    Claudio Weiler - special thanks for ideas and help related to implementation of human-readable/numeric ordering for branches and tags (see bug #390467)
  *******************************************************************************/
 
 package org.eclipse.team.svn.core.operation.remote;
@@ -117,6 +118,8 @@ public class GetRemoteFolderChildrenOperation extends AbstractActionOperation {
 		Object [][]sortableData = tmpSortableData.toArray(new Object[tmpSortableData.size()][]);
 
 		Arrays.sort(sortableData, new Comparator<Object []>() {
+			private NaturalComparator comparator = new NaturalComparator();
+			
 			public int compare(Object []firstArray, Object []secondArray) {
 				IRepositoryResource first = (IRepositoryResource)firstArray[1], second = (IRepositoryResource)secondArray[1];
 				String firstExtName = (String)firstArray[0], secondExtName = (String)secondArray[0];
@@ -136,7 +139,7 @@ public class GetRemoteFolderChildrenOperation extends AbstractActionOperation {
 			private int compareNames(IRepositoryResource first, String firstExtName, IRepositoryResource second, String secondExtName) {
 				String firstName = firstExtName != null ? firstExtName : first.getName();
 				String secondName = secondExtName != null ? secondExtName : second.getName();
-				return GetRemoteFolderChildrenOperation.this.caseInsensitive ? firstName.compareToIgnoreCase(secondName) : firstName.compareTo(secondName);
+				return this.comparator.compare(firstName, secondName);
 			}
 			
 			private int compareRoots(int firstKind, int secondKind) {
@@ -151,5 +154,82 @@ public class GetRemoteFolderChildrenOperation extends AbstractActionOperation {
 			this.extNames[i] = (String)sortableData[i][0];
 		}
 	}
+	
+	private int compareStrings(String firstName, String secondName)
+	{
+		return this.caseInsensitive ? firstName.compareToIgnoreCase(secondName) : firstName.compareTo(secondName);
+	}
 
+	private class NaturalComparator implements Comparator<String> {
+		public int compare(String o1, String o2) {
+			// equal string, ignore all the rest
+			if (GetRemoteFolderChildrenOperation.this.compareStrings(o1, o2) == 0) {
+				return 0;
+			}
+			// empty string is lower
+			if (o1.length() == 0) {
+				return -1;
+			}
+			if (o2.length() == 0) {
+				return 1;
+			}
+			
+			// from now we consider a 'part' a sequence of digits or non-digits 
+
+			// extract first part of o1
+			String o1Part = "";
+			int o1i = 0;
+			boolean o1IsDigit = Character.isDigit(o1.charAt(o1i++));
+			for (; o1i < o1.length(); o1i++) {
+				if (Character.isDigit(o1.charAt(o1i)) != o1IsDigit) {
+					break;
+				}
+			}
+			o1Part = o1.substring(0, o1i);
+
+			// extract first part of o2
+			String o2Part = "";
+			int o2i = 0;
+			boolean o2IsDigit = Character.isDigit(o2.charAt(o2i++));
+			for (; o2i < o2.length(); o2i++) {
+				if (Character.isDigit(o2.charAt(o2i)) != o2IsDigit) {
+					break;
+				}
+			}
+			o2Part = o2.substring(0, o2i);
+
+			// compare both parts
+			int result = 0;
+			if (o1IsDigit && o2IsDigit) {
+				if (o1Part.charAt(0) == '0' && o2Part.charAt(0) != '0') {
+					result = -1;
+				}
+				else if (o2Part.charAt(0) == '0' && o1Part.charAt(0) != '0') {
+					result = 1;
+				}
+				else {
+					// if both parts are number, then numeric test
+					try {
+						int n1 = Integer.parseInt(o1Part);
+						int n2 = Integer.parseInt(o2Part);
+						// if numbers are equal, make numeric test, else use string test
+						result = n1 != n2 ? (n1 < n2 ? -1 : 1) : 0;
+						if (result == 0 && o1Part.length() != o2Part.length()) {
+							// leading zeros are relevant: more zeros first
+							result = o1Part.length() < o2Part.length() ? 1 : -1;
+						}
+					} 
+					catch (NumberFormatException nbe) {
+						// should not enter here as we test for numeric parts
+					}
+				}
+			}
+			else {
+				result = GetRemoteFolderChildrenOperation.this.compareStrings(o1Part, o2Part);
+			}
+			
+			// if parts aren't equal return, otherwise continue test for the rest of the string
+			return result != 0 ? result : this.compare(o1.substring(o1i), o2.substring(o2i));
+		}
+	}
 }
