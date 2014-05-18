dbcb5166713c674fcd66dc2a2003540ecb0959e2
Alexander Gurov <a.gurov@i-ween.com>
2014-05-18 20:41:39 +0000

    Delete file from SVN not work always (bug 434944) https://bugs.eclipse.org/bugs/show_bug.cgi?id=434944

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/operation/local/MarkAsMergedOperation.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/operation/local/MarkAsMergedOperation.java
@@ -139,7 +139,8 @@ public class MarkAsMergedOperation extends AbstractWorkingCopyOperation implemen
 		this.doOperation(new UpdateOperation(new IResource[] {local.getResource()}, this.getRevisionToUpdate(local), this.ignoreExternals), monitor);
 		//don't delete the resource which already doesn't exist on file system
 		//this can happen with tree conflicts, for instance, local - delete and remote - delete
-		if (local.getResource().exists()) {
+		File f = new File(FileUtility.getWorkingCopyPath(local.getResource()));
+		if (f.exists()) {
 			this.doOperation(new DeleteResourceOperation(local.getResource()), monitor);	
 		}
 	}
