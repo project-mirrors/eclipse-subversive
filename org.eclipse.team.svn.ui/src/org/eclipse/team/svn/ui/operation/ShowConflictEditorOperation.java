69cc5bde675974a33266ec21148bdaceb7ff9ef3
Alexander Gurov <a.gurov@i-ween.com>
2015-05-29 07:45:41 +0000

    File content comparison editor shouldn't be opened for conflicted folders [ID-HJ1S2] (bug 468639) https://bugs.eclipse.org/bugs/show_bug.cgi?id=468639

--- org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/operation/ShowConflictEditorOperation.java
+++ org.eclipse.team.svn.ui/src/org/eclipse/team/svn/ui/operation/ShowConflictEditorOperation.java
@@ -142,7 +142,9 @@ public class ShowConflictEditorOperation extends AbstractWorkingCopyOperation {
 		}
 		
 		// open compare editor if required
-		if (status.length == 1 && status[0].hasConflict && status[0].treeConflicts != null && status[0].treeConflicts[0].remotePath != null && status[0].treeConflicts[0].basePath != null) {
+		if (resource.getType() == IResource.FILE && 
+			status.length == 1 && status[0].hasConflict && status[0].treeConflicts != null && 
+			status[0].treeConflicts[0].remotePath != null && status[0].treeConflicts[0].basePath != null) {
 			IContainer parent = resource.getParent();
 			parent.refreshLocal(IResource.DEPTH_ONE, monitor);
 			/*
