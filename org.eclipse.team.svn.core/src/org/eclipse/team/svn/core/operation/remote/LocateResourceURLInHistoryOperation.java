61acc040b8e459c6358f2a094c4e4355d397d63a
Alexander Gurov <a.gurov@i-ween.com>
2016-04-25 20:40:41 +0000

    LocateResourceURLInHistoryOperation should use the same code as the one that is used by connectors (bug 492402) https://bugs.eclipse.org/bugs/show_bug.cgi?id=492402

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/operation/remote/LocateResourceURLInHistoryOperation.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/operation/remote/LocateResourceURLInHistoryOperation.java
@@ -12,12 +12,9 @@
 package org.eclipse.team.svn.core.operation.remote;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.team.svn.core.SVNMessages;
 import org.eclipse.team.svn.core.connector.ISVNConnector;
-import org.eclipse.team.svn.core.connector.SVNLogEntry;
-import org.eclipse.team.svn.core.connector.SVNLogPath;
-import org.eclipse.team.svn.core.connector.SVNRevision;
+import org.eclipse.team.svn.core.connector.SVNEntryRevisionReference;
 import org.eclipse.team.svn.core.connector.SVNRevision.Kind;
 import org.eclipse.team.svn.core.operation.IUnprotectedOperation;
 import org.eclipse.team.svn.core.operation.SVNProgressMonitor;
@@ -59,10 +56,7 @@ public class LocateResourceURLInHistoryOperation extends AbstractRepositoryOpera
 			if (this.converted[i].getSelectedRevision().getKind() == Kind.NUMBER) {
 				this.protectStep(new IUnprotectedOperation() {
 					public void run(IProgressMonitor monitor) throws Exception {
-						IRepositoryResource result = LocateResourceURLInHistoryOperation.this.processEntry(LocateResourceURLInHistoryOperation.this.converted[idx], monitor);
-						LocateResourceURLInHistoryOperation.this.converted[idx] = LocateResourceURLInHistoryOperation.this.converted[idx] == result ? SVNUtility.copyOf(result) : result;
-						// when URL is corrected peg can be set to selected revision number
-						LocateResourceURLInHistoryOperation.this.converted[idx].setPegRevision(LocateResourceURLInHistoryOperation.this.converted[idx].getSelectedRevision());
+						LocateResourceURLInHistoryOperation.this.converted[idx] = LocateResourceURLInHistoryOperation.this.processEntry(LocateResourceURLInHistoryOperation.this.converted[idx], monitor);
 					}
 				}, monitor, resources.length);
 			}
@@ -72,74 +66,17 @@ public class LocateResourceURLInHistoryOperation extends AbstractRepositoryOpera
 	protected IRepositoryResource processEntry(IRepositoryResource current, IProgressMonitor monitor) throws Exception {
 		IRepositoryLocation location = current.getRepositoryLocation();
 		ISVNConnector proxy = location.acquireSVNProxy();
-		SVNLogEntry []msgs = null;
-		int index = 0;
 		try {
-			IRepositoryResource pegNode = SVNUtility.copyOf(current);
-			pegNode.setSelectedRevision(pegNode.getPegRevision());
-			if (pegNode.exists()) {
-				msgs = SVNUtility.logEntries(proxy, SVNUtility.getEntryReference(pegNode), SVNRevision.fromNumber(0), pegNode.getPegRevision(), ISVNConnector.Options.STOP_ON_COPY | ISVNConnector.Options.DISCOVER_PATHS, ISVNConnector.EMPTY_LOG_ENTRY_PROPS, 1, new SVNProgressMonitor(this, monitor, null));
-			}
-			else if (pegNode.getParent() != null) {
-				msgs = SVNUtility.logEntries(proxy, SVNUtility.getEntryReference(pegNode.getParent()), SVNRevision.fromNumber(0), pegNode.getPegRevision(), ISVNConnector.Options.STOP_ON_COPY | ISVNConnector.Options.DISCOVER_PATHS, ISVNConnector.EMPTY_LOG_ENTRY_PROPS, 1, new SVNProgressMonitor(this, monitor, null));
-				if (msgs != null) {
-					for (int j = 0; j < msgs.length; j++) {
-						SVNLogPath []paths = msgs[j].changedPaths;
-						if (paths != null) {
-							for (int k = 0; k < paths.length; k++) {
-								if (paths[k] != null && current.getUrl().endsWith(paths[k].path)) {
-									index = j;
-									break;
-								}
-							}
-						}
-					}
-				}
-			}
+			SVNEntryRevisionReference entry = SVNUtility.getEntryRevisionReference(current);
+			entry = SVNUtility.convertRevisionReference(proxy, entry, new SVNProgressMonitor(this, monitor, null));
+			IRepositoryResource retVal = current instanceof IRepositoryFile ? (IRepositoryResource)location.asRepositoryFile(entry.path, false) : location.asRepositoryContainer(entry.path, false);
+			retVal.setPegRevision(entry.pegRevision);
+			retVal.setSelectedRevision(entry.revision);
+			return retVal;
 		}
 		finally {
 			location.releaseSVNProxy(proxy);
 		}
-		if (msgs != null && msgs.length > index && msgs[index] != null) {
-			SVNLogPath []paths = msgs[index].changedPaths;
-			if (paths == null) {
-				return current;
-			}
-			Path pattern = new Path(current.getUrl().substring(location.getRepositoryRootUrl().length()));
-			int idx = -1;
-			int prefLen = 0;
-			for (int i = 0; i < paths.length; i++) {
-				Path path = new Path(paths[i].path);
-				int tLen = path.segmentCount();
-				if (path.isPrefixOf(pattern) && paths[i].copiedFromPath != null && tLen > prefLen) {
-					idx = i;
-				}
-			}
-			if (idx == -1) {
-				return current;
-			}
-			String copiedFrom = location.getRepositoryRootUrl() + paths[idx].copiedFromPath + pattern.toString().substring(paths[idx].path.length());
-			
-			long rev = paths[idx].copiedFromRevision;
-			SVNRevision searchRevision = current.getSelectedRevision();
-			
-			if (searchRevision.getKind() != SVNRevision.Kind.NUMBER) {
-				throw new RuntimeException("Unexpected revision kind. Kind: " + searchRevision.getKind());
-			}
-			long searchRev = ((SVNRevision.Number)searchRevision).getNumber();
-			if (rev < searchRev) {
-				if (msgs[index].revision <= searchRev) {
-					return current;
-				}
-				searchRevision = SVNRevision.fromNumber(rev);
-			}
-			
-			IRepositoryResource retVal = current instanceof IRepositoryFile ? (IRepositoryResource)location.asRepositoryFile(copiedFrom, false) : location.asRepositoryContainer(copiedFrom, false);
-			retVal.setPegRevision(SVNRevision.fromNumber(rev));
-			retVal.setSelectedRevision(searchRevision);
-			return rev <= searchRev ? retVal : this.processEntry(retVal, monitor);
-		}
-		return current;
 	}
 
 }
