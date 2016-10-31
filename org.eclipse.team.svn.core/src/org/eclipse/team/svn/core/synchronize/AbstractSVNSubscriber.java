92ad1ca1bd71f225e2c94048e2bca40c75517fd1
Alexander Gurov <a.gurov@i-ween.com>
2016-10-31 09:37:18 +0000

    Performance improvements for AbstractSVNSubscriber (bug 506777) https://bugs.eclipse.org/bugs/show_bug.cgi?id=506777

--- org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/synchronize/AbstractSVNSubscriber.java
+++ org.eclipse.team.svn.core/src/org/eclipse/team/svn/core/synchronize/AbstractSVNSubscriber.java
@@ -253,12 +253,12 @@ public abstract class AbstractSVNSubscriber extends Subscriber implements IResou
 	
     public void resourcesStateChanged(ResourceStatesChangedEvent event) {
     	try {
-    		if (event.type == ResourceStatesChangedEvent.CHANGED_NODES) {    		
+    		if (event.type == ResourceStatesChangedEvent.CHANGED_NODES) {    	
+    			// TODO: should we expand *entire* projects here???
 				this.resourcesStateChangedImpl(event.getResourcesRecursivelly());
     		}
 		} catch (TeamException e) {
 			LoggedOperation.reportError(this.getClass().getName(), e);
-			
 		}    	
     }
     
@@ -283,9 +283,17 @@ public abstract class AbstractSVNSubscriber extends Subscriber implements IResou
 	}
 	
     protected void resourcesStateChangedImpl(IResource []resources) throws TeamException {
-    	Set<IResource> allResources = new HashSet<IResource>(Arrays.asList(resources));
-    	for (int i = 0; i < resources.length; i++) {
-    		allResources.addAll(Arrays.asList(this.statusCache.allMembers(resources[i])));
+    	Set<IResource> allResources = new HashSet<IResource>(resources.length);
+    	for (IResource resource : resources) {
+    		allResources.add(resource);
+
+    		// this returns *all* members, also ignored too
+			IResource[] allMembers = this.statusCache.allMembers(resource);
+    		if (allMembers.length > 0) {
+    			for (IResource child : allMembers) {
+    				allResources.add(child);
+				}
+    		}
     	}
     	synchronized (this.oldResources) {
     		for (Iterator<IResource> it = this.oldResources.iterator(); it.hasNext(); ) {
@@ -295,17 +303,22 @@ public abstract class AbstractSVNSubscriber extends Subscriber implements IResou
 				 * I investigated the code but couldn't find the better way how to fix this problem.
 				 * TODO Possibly, there's a better solution for it.
 				 */
-				if (resource.getLocation() != null) {
+				if (resource.getLocation() != null && !allResources.contains(resource)) {
 					SVNChangeStatus status = SVNUtility.getSVNInfoForNotConnected(resource);
 					if (status == null || (status.textStatus != SVNEntryStatus.Kind.DELETED && status.textStatus != SVNEntryStatus.Kind.MISSING)) {
 						allResources.add(resource);
 					}	
 				}
 			}
+    		if (allResources.isEmpty()) {
+    			return;
+    		}
         	IResource []refreshSet = allResources.toArray(new IResource[allResources.size()]);
-        	// ensure we cached all locally-known resources
+        	// ensure we cached all locally-known resources (used in pair with asLocalResourceDirty() in getSyncInfo())
+        	//	TODO best case - the code should not exists. Needs to be verified if there is a different solution.
         	if (CoreExtensionsManager.instance().getOptionProvider().is(IOptionProvider.SVN_CACHE_ENABLED)) {
         		IResource []parents = FileUtility.getParents(refreshSet, false);
+        		FileUtility.reorder(parents, true); //ensure the proper load order, so that there is no performance overhead
             	for (int i = 0; i < parents.length; i++) {
             		try {
     					SVNRemoteStorage.instance().getRegisteredChildren((IContainer)parents[i]);
